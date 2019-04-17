95f4915a679785535b91e7cdd07e55979b003c00
xueyongwei <xueyongwei@ylmf.com>
2019-04-17 10:09:26 +0800

    Initial Commit

---
+++ YRCollectionViewLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YRCollectionViewLayout
+//
+//  Created by 薛永伟 on 2019/4/17.
+//  Copyright © 2019 Yuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
